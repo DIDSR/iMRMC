d4786c112238ad2ec0a737cd9035c20c7434bc5e
Qi Gong <qi.gong@fda.hhs.gov>
2017-11-30 11:16:26 -0500

    upload full iRoemetz version html and pdf manuals

--- 000_iRoeMetz/userManualHTML/js/drexplain.data.index.js
+++ 000_iRoeMetz/userManualHTML/js/drexplain.data.index.js
@@ -3,10 +3,10 @@ DR_EXPLAIN.data_index = {
 
 	// index
 	DREX_NODE_KEYWORDS: [],
-	DREX_NODE_KEYWORDS_START: [0,0], //length:= drex.nodes_count,
-	DREX_NODE_KEYWORDS_END: [0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
 
-	DREX_KEYWORD_NAMES: ["<NEW K... Advanced License is required!"],
+	DREX_KEYWORD_NAMES: ["<NEW KEYWORD>"],
 	DREX_KEYWORD_CHILD_START: [1],
 	DREX_KEYWORD_CHILD_END: [1]
 	
