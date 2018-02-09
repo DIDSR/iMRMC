bccc1d6bcb84080f26e3726ce6194d8923efcfe9
Brandon Gallas <brandon.gallas@fda.hhs.gov>
2018-02-09 17:36:27 -0500

    validate_uStat11b.R had too many things going on  * validate the within-reader, between-modality limits of agreement  * validate the between-reader, within-modality limits of agreement  * validate the between-reader, between-modality limits of agreement Each of these validations is given it's own script. validate_laWRBM.R is done and the others are not.

---
+++ Rpackage/iMRMC/futureVignettes/validate_laWRBM.R
@@ -0,0 +1,120 @@
+#### Simulation set up ####
+library(iMRMC)
+init.lecuyerRNG()
+
+# Create an MRMC data frame
+# refer to Gallas2014_J-Med-Img_v1p031006
+simRoeMetz.config <- sim.gRoeMetz.config()
+simRoeMetz.config$nC.neg <- 20
+simRoeMetz.config$nC.pos <- 20
+
+startTime <- proc.time()[1]
+nMC <- 1000
+
+#### Loop over MC trials ####
+df.sim1obs <- data.frame()
+df.laWRBM <- data.frame()
+for (i in 1:nMC) {
+
+  # Simulate data
+  df.MRMC <- sim.gRoeMetz(simRoeMetz.config)
+  df.MRMC <- undoIMRMCdf(df.MRMC)
+  df.MRMC <- split(df.MRMC, list(df.MRMC$modalityID, df.MRMC$truth))
+
+  # For each monte carlo trial, the observations are the scores
+  # from two readers reading the same case and their difference.
+  # This simulation attempts to find the variance of these scores and their difference.
+  # This is a little different from other work where each monte carlo trial yields
+  # a reader and case averaged quantity.
+  Ar1c1 <- df.MRMC$testA.1[
+    df.MRMC$testA.1$readerID == "reader1" &
+      df.MRMC$testA.1$caseID == "posCase1", "score"]
+  Ar2c1 <- df.MRMC$testA.1[
+    df.MRMC$testA.1$readerID == "reader2" &
+      df.MRMC$testA.1$caseID == "posCase1", "score"]
+  Br1c1 <- df.MRMC$testB.1[
+    df.MRMC$testB.1$readerID == "reader1" &
+      df.MRMC$testB.1$caseID == "posCase1", "score"]
+  Br2c1 <- df.MRMC$testB.1[
+    df.MRMC$testB.1$readerID == "reader2" &
+      df.MRMC$testB.1$caseID == "posCase1", "score"]
+
+  df.sim1obs <- rbind(
+    df.sim1obs,
+    data.frame(
+      Ar1c1 = Ar1c1,
+      Ar2c1 = Ar2c1,
+      Br1c1 = Br1c1,
+      Br2c1 = Br2c1,
+
+      # WRBM
+      Ar1c1minusBr1c1 = Ar1c1 - Br1c1,
+      Ar2c1minusBr2c1 = Ar2c1 - Br2c1,
+
+      # BRWM
+      Ar1c1minusAr2c1 = Ar1c1 - Ar2c1,
+      Br1c1minusBr2c1 = Br1c1 - Br2c1,
+
+      # BRBM
+      Ar1c1minusBr2c1 = Ar1c1 - Br2c1,
+      Ar2c1minusBr1c1 = Ar2c1 - Br1c1,
+
+      # BRBM symmetric
+      Ar1c1minusBr2c1.symmetric = 0.5 * (Ar1c1 - Br2c1 + Ar2c1 - Br1c1)
+    )
+  )
+
+  # Estimate the WRBM limits of agreement
+  result.laWRBM <- laWRBM(
+    rbind(df.MRMC$testA.1, df.MRMC$testB.1),
+    keyColumns = c("readerID", "caseID", "modalityID", "score"),
+    modalitiesToCompare = c("testA", "testB", "testA", "testB"))
+
+  # Aggregate the results of all the MC trials into one data frame.
+  df.laWRBM <- rbind(
+    df.laWRBM,
+    data.frame(
+      var.Ar1cminusBr1c = result.laWRBM$var[1]
+    )
+  )
+
+}
+
+#### Summarize MC simulation ####
+# Take the mean and the variance of the observations, including WRBM differences
+df.sim1obs.mcMean <- colMeans(df.sim1obs)
+names(df.sim1obs.mcMean) <- paste(names(df.sim1obs.mcMean), ".", "mcMean", sep = "")
+df.sim1obs.mcVar <- diag(cov(df.sim1obs))
+names(df.sim1obs.mcVar) <- paste(names(df.sim1obs.mcVar), ".", "mcVar", sep = "")
+
+# Estimate the variance of the limits of aggreement from an MRMC data set
+df.laWRBM.mcMean <- colMeans(df.laWRBM)
+names(df.laWRBM.mcMean) <- paste(names(df.laWRBM.mcMean), ".", "mcMean", sep = "")
+df.laWRBM.mcVar <- diag(cov(df.laWRBM))
+names(df.laWRBM.mcVar) <- paste(names(df.laWRBM.mcVar), ".", "mcVar", sep = "")
+
+#### Print Results ####
+# Print the number of MC trials and the experimental size of each MC trial
+print("")
+desc <- data.frame(nMC = nMC, nR = simRoeMetz.config$nR, nC = simRoeMetz.config$nC.pos)
+print(paste("nMC = ", nMC))
+
+# Print the time to complete this MC simulation
+endTime <- proc.time()[1]
+print(paste("Time to complete", nMC, "observations = ", endTime - startTime))
+
+print("")
+print("MCmean of single observations")
+print(df.sim1obs.mcMean[1:4])
+
+print("")
+print("MCmean of within-reader between-modality differences")
+print(df.sim1obs.mcMean[5:6])
+
+print("")
+print("MCvar of within-reader and between-modality differences")
+print("df.sim1obs.mcVar[5:6]")
+print(df.sim1obs.mcVar[5:6])
+print("df.laWRBM.mcMean[1]")
+print(df.laWRBM.mcMean[1])
+
