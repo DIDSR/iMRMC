4d2a0a8f0e782646d3043427d837fbeb025224e8
Qi Gong <qi.gong@fda.hhs.gov>
2020-01-10 15:31:55 -0500

    Solve old version of Java problem:Solve old version of Java problem: In test_doIMRMC.R, check whether the iMRMC.jar succeed. If not, return error and stop the code.

--- Rpackage/iMRMC/tests/testthat/test_doIMRMC.R
+++ Rpackage/iMRMC/tests/testthat/test_doIMRMC.R
@@ -22,36 +22,40 @@ workDir <- file.path("tests", "testthat")
 workDir <- NULL
 
 # Analyze the input file
+options(warn=-1)
 result <- doIMRMC(df.MRMC, workDir = workDir, stripDatesForTests = TRUE)
-saveResult <- result
-
-cat("\n")
-print(result$Ustat[, 1:10])
-cat("\n")
-print(result$Ustat[, c(1:6, 11:12)])
-cat("\n")
-print(result$Ustat[, c(1:6, 17:21)])
-
-toc <- proc.time()
-cat("\n  Elapsed time: ", toc[1:3] - tic[1:3], "\n")
-
-#### TEST ####
-
-# Save the result to a file for future comparisons
-fileName <- "test_doIMRMC.Rdata"
-if (flagSave) {
-  save(saveResult, file = file.path("tests", "testthat", fileName))
-}
-
-# Recover the expected results
-saveResult <- 0
-if (!file.exists(fileName)) {
-  fileName <- file.path("tests", "testthat", fileName)
-}
-load(fileName)
-
-test_that(
-  "doIMRMC does not change", {
-    expect_equal(saveResult, result,tolerance=1e-5)
+options(warn=0)
+if (result$error == 1){
+  
+}else{
+  saveResult <- result
+  cat("\n")
+  print(result$Ustat[, 1:10])
+  cat("\n")
+  print(result$Ustat[, c(1:6, 11:12)])
+  cat("\n")
+  print(result$Ustat[, c(1:6, 17:21)])
+  
+  toc <- proc.time()
+  cat("\n  Elapsed time: ", toc[1:3] - tic[1:3], "\n")
+  
+  #### TEST ####
+  
+  # Save the result to a file for future comparisons
+  fileName <- "test_doIMRMC.Rdata"
+  if (flagSave) {
+    save(saveResult, file = file.path("tests", "testthat", fileName))
   }
-)
+  # Recover the expected results
+  saveResult <- 0
+  if (!file.exists(fileName)) {
+    fileName <- file.path("tests", "testthat", fileName)
+  }
+  load(fileName)
+  test_that(
+    "doIMRMC does not change", {
+      expect_equal(saveResult, result,tolerance=1e-5)
+    }
+  )
+}
+
