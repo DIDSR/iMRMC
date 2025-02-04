97e6abd9625b2d3d205348b32c2facbe10f1c1d4
Brandon Gallas <brandon.gallas@fda.hhs.gov>
2025-02-04 14:29:43 -0500

    WIP: Fix bug issue 190: Uncomment all test elements for unpaired readers - successful

--- Rpackage/iMRMC/tests/testthat/test_doIMRMC_R4.R
+++ Rpackage/iMRMC/tests/testthat/test_doIMRMC_R4.R
@@ -74,48 +74,48 @@ print(end.time - start.time)
 # Analyze the MRMC ROC data with legacy code
 saveData <- FALSE
 if (saveData) {
-
+  
   # Results from legacy code are not available or need to be updated
   start.time <- proc.time()
-
+  
   # Processing time
   # user  system elapsed
   # 0.07    0.04    3.17
   targetTest3 <- iMRMC::doIMRMC_java(dFrame.imrmc)
   save(targetTest3,
        file = file.path("tests", "testthat", "test_doIMRMC_R4.rda"))
-
+  
   end.time <- proc.time()
   print("Runtime for legacy code created")
   print(end.time - start.time)
-
+  
   browser()
-
+  
 } else {
-
+  
   # Results from legacy code need to be read from the package
   # There are two modes for running: standard and test
-
+  
   # The working directory when running in standard mode is the package root
   temp <- list.files(
     path = file.path("tests", "testthat", "."),
     pattern = "test_doIMRMC_R4.rda")
-
+  
   if (length(temp) > 0) {
     if (temp == "test_doIMRMC_R4.rda")
       load(file.path("tests", "testthat", "test_doIMRMC_R4.rda"))
   }
-
+  
   # The working directory when running in test mode is the tests/testthat directory
   temp <- list.files(
     path = ".",
     pattern = "test_doIMRMC_R4.rda")
-
+  
   if (length(temp) > 0) {
     if ( temp == "test_doIMRMC_R4.rda")
       load(file.path(".", "test_doIMRMC_R4.rda"))
   }
-
+  
 }
 result_doIMRMC_target <- targetTest3
 
@@ -166,153 +166,153 @@ testthat::test_that(
 
 
 
-# # MLEstat output ###############
-# target = result_doIMRMC_target$MLEstat[,4:24]
-# current = result_doIMRMC_current$MLEstat
-# 
-# testthat::test_that(
-#   "doIMRMC MLEstat output does not change", {
-#     testthat::expect_equal(target, current)
-#   }
-# )
-# 
-# 
-# 
-# # varDecomp$BDG #############################################################
-# target <- result_doIMRMC_target$varDecomp$BDG
-# current <- result_doIMRMC_current$varDecomp$BDG
-# 
-# # Current has new simpler format
-# # Reduce target to this format
-# target$Ustat$comp <- target$Ustat$comp$testA.testB
-# rownames(target$Ustat$comp) <- NULL
-# target$Ustat$coeff <- target$Ustat$coeff$testA.testB
-# rownames(target$Ustat$coeff) <- NULL
-# target$MLE$comp <- target$MLE$comp$testA.testB
-# rownames(target$MLE$comp) <- NULL
-# target$MLE$coeff <- target$MLE$coeff$testA.testB
-# rownames(target$MLE$coeff) <- NULL
-# 
-# 
-# 
-# # Current does not have columns modalityID.1 and modalityID.2
-# current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.1")
-# current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.2")
-# current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.1")
-# current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.2")
-# current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.1")
-# current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.2")
-# current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.1")
-# current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.2")
-# 
-# 
-# 
-# # Current coefficients yield the covariance.
-# # Target coefficients yield 2*covariance.
-# current$Ustat$coeff[3, ] <- 2 * current$Ustat$coeff[3, ]
-# current$MLE$coeff[3, ] <- 2 * current$MLE$coeff[3, ]
-# 
-# 
-# 
-# # R code has 7 digits significance
-# # Java code has 6 digits significance
-# # The reason is that the Java function reads and writes files.  
-# testthat::test_that(
-#   "doIMRMC varcomp-BDG output does not change", {
-#     testthat::expect_equal(target, current,tolerance = 1e-4)
-#   }
-# )
-# 
-# 
-# 
-# # varDecomp$BCK #############################################################
-# target <- result_doIMRMC_target$varDecomp$BCK
-# current <- result_doIMRMC_current$varDecomp$BCK
-# 
-# 
-# 
-# # Current has new simpler format
-# # Reduce target to this format
-# target$Ustat$comp <- target$Ustat$comp$testA.testB
-# rownames(target$Ustat$comp) <- NULL
-# target$Ustat$coeff <- target$Ustat$coeff$testA.testB
-# rownames(target$Ustat$coeff) <- NULL
-# target$MLE$comp <- target$MLE$comp$testA.testB
-# rownames(target$MLE$comp) <- NULL
-# target$MLE$coeff <- target$MLE$coeff$testA.testB
-# rownames(target$MLE$coeff) <- NULL
-# 
-# 
-# 
-# # Current does not have columns modalityID.1 and modalityID.2
-# current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.1")
-# current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.2")
-# current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.1")
-# current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.2")
-# current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.1")
-# current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.2")
-# current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.1")
-# current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.2")
-# 
-# 
-# 
-# # Current coefficients yield the covariance.
-# # Target coefficients yield 2*covariance.
-# current$Ustat$coeff[3, ] <- 2 * current$Ustat$coeff[3, ]
-# current$MLE$coeff[3, ] <- 2 * current$MLE$coeff[3, ]
-# 
-# 
-# 
-# # R code has 7 digits significance
-# # Java code has 6 digits significance
-# # The reason is that the Java function reads and writes files.  
-# testthat::test_that(
-#   "doIMRMC varcomp-BCK output does not change", {
-#     testthat::expect_equal(target, current,tolerance = 1e-4)
-#   }
-# )
-# 
-# 
-# 
-# # ROC ###############
-# target <- result_doIMRMC_target$ROC
-# current <- result_doIMRMC_current$ROC
-# 
-# # ROC per-reader visual test
-# plot(current$testA.reader2$fpf,
-#      current$testA.reader2$tpf, type = "l")
-# lines(target$`testA: reader2`$fpf,
-#       target$`testA: reader2`$tpf, lty = 2)
-# 
-# # ROC pooled-readers visual test
-# plot(current$testA.pooled$fpf,
-#      current$testA.pooled$tpf, type = "l")
-# lines(target$`testA: Pooled Average`$fpf,
-#       target$`testA: Pooled Average`$tpf, lty = 2)
-# 
-# # ROC diagonal average visual test
-# plot(current$testA.diagonalAvg$fpf,
-#      current$testA.diagonalAvg$tpf, type = "l")
-# lines(target$`testA: Diagonal Average`$fpf,
-#       target$`testA: Diagonal Average`$tpf, lty = 2)
-# 
-# # ROC vertical average visual test
-# plot(current$testA.verticalAvg$fpf,
-#      current$testA.verticalAvg$tpf, type = "l")
-# lines(target$`testA: Vertical Average`$fpf,
-#       target$`testA: Vertical Average`$tpf, lty = 2)
-# 
-# # ROC horizontal average test
-# plot(current$testA.horizontalAvg$fpf,
-#      current$testA.horizontalAvg$tpf, type = "l")
-# lines(target$`testA: Horizontal Average`$fpf,
-#       target$`testA: Horizontal Average`$tpf, lty = 2)
-# 
-# # plot(current$testB.reader1$fpf,
-# #      current$testB.reader1$tpf)
-# # lines(current$testB.reader2$fpf,
-# #      current$testB.reader2$tpf)
-# # lines(current$testB.reader3$fpf,
-# #       current$testB.reader3$tpf)
-# # lines(current$testB.reader4$fpf,
-# #       current$testB.reader4$tpf)
+# MLEstat output ###############
+target = result_doIMRMC_target$MLEstat[,4:24]
+current = result_doIMRMC_current$MLEstat
+
+testthat::test_that(
+  "doIMRMC MLEstat output does not change", {
+    testthat::expect_equal(target, current)
+  }
+)
+
+
+
+# varDecomp$BDG #############################################################
+target <- result_doIMRMC_target$varDecomp$BDG
+current <- result_doIMRMC_current$varDecomp$BDG
+
+# Current has new simpler format
+# Reduce target to this format
+target$Ustat$comp <- target$Ustat$comp$testA.testB
+rownames(target$Ustat$comp) <- NULL
+target$Ustat$coeff <- target$Ustat$coeff$testA.testB
+rownames(target$Ustat$coeff) <- NULL
+target$MLE$comp <- target$MLE$comp$testA.testB
+rownames(target$MLE$comp) <- NULL
+target$MLE$coeff <- target$MLE$coeff$testA.testB
+rownames(target$MLE$coeff) <- NULL
+
+
+
+# Current does not have columns modalityID.1 and modalityID.2
+current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.1")
+current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.2")
+current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.1")
+current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.2")
+current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.1")
+current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.2")
+current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.1")
+current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.2")
+
+
+
+# Current coefficients yield the covariance.
+# Target coefficients yield 2*covariance.
+current$Ustat$coeff[3, ] <- 2 * current$Ustat$coeff[3, ]
+current$MLE$coeff[3, ] <- 2 * current$MLE$coeff[3, ]
+
+
+
+# R code has 7 digits significance
+# Java code has 6 digits significance
+# The reason is that the Java function reads and writes files.
+testthat::test_that(
+  "doIMRMC varcomp-BDG output does not change", {
+    testthat::expect_equal(target, current,tolerance = 1e-4)
+  }
+)
+
+
+
+# varDecomp$BCK #############################################################
+target <- result_doIMRMC_target$varDecomp$BCK
+current <- result_doIMRMC_current$varDecomp$BCK
+
+
+
+# Current has new simpler format
+# Reduce target to this format
+target$Ustat$comp <- target$Ustat$comp$testA.testB
+rownames(target$Ustat$comp) <- NULL
+target$Ustat$coeff <- target$Ustat$coeff$testA.testB
+rownames(target$Ustat$coeff) <- NULL
+target$MLE$comp <- target$MLE$comp$testA.testB
+rownames(target$MLE$comp) <- NULL
+target$MLE$coeff <- target$MLE$coeff$testA.testB
+rownames(target$MLE$coeff) <- NULL
+
+
+
+# Current does not have columns modalityID.1 and modalityID.2
+current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.1")
+current$Ustat$comp <- deleteCol(current$Ustat$comp, "modalityID.2")
+current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.1")
+current$Ustat$coeff <- deleteCol(current$Ustat$coeff, "modalityID.2")
+current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.1")
+current$MLE$comp <- deleteCol(current$MLE$comp, "modalityID.2")
+current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.1")
+current$MLE$coeff <- deleteCol(current$MLE$coeff, "modalityID.2")
+
+
+
+# Current coefficients yield the covariance.
+# Target coefficients yield 2*covariance.
+current$Ustat$coeff[3, ] <- 2 * current$Ustat$coeff[3, ]
+current$MLE$coeff[3, ] <- 2 * current$MLE$coeff[3, ]
+
+
+
+# R code has 7 digits significance
+# Java code has 6 digits significance
+# The reason is that the Java function reads and writes files.
+testthat::test_that(
+  "doIMRMC varcomp-BCK output does not change", {
+    testthat::expect_equal(target, current,tolerance = 1e-4)
+  }
+)
+
+
+
+# ROC ###############
+target <- result_doIMRMC_target$ROC
+current <- result_doIMRMC_current$ROC
+
+# ROC per-reader visual test
+plot(current$testA.reader2$fpf,
+     current$testA.reader2$tpf, type = "l")
+lines(target$`testA: reader2`$fpf,
+      target$`testA: reader2`$tpf, lty = 2)
+
+# ROC pooled-readers visual test
+plot(current$testA.pooled$fpf,
+     current$testA.pooled$tpf, type = "l")
+lines(target$`testA: Pooled Average`$fpf,
+      target$`testA: Pooled Average`$tpf, lty = 2)
+
+# ROC diagonal average visual test
+plot(current$testA.diagonalAvg$fpf,
+     current$testA.diagonalAvg$tpf, type = "l")
+lines(target$`testA: Diagonal Average`$fpf,
+      target$`testA: Diagonal Average`$tpf, lty = 2)
+
+# ROC vertical average visual test
+plot(current$testA.verticalAvg$fpf,
+     current$testA.verticalAvg$tpf, type = "l")
+lines(target$`testA: Vertical Average`$fpf,
+      target$`testA: Vertical Average`$tpf, lty = 2)
+
+# ROC horizontal average test
+plot(current$testA.horizontalAvg$fpf,
+     current$testA.horizontalAvg$tpf, type = "l")
+lines(target$`testA: Horizontal Average`$fpf,
+      target$`testA: Horizontal Average`$tpf, lty = 2)
+
+# plot(current$testB.reader1$fpf,
+#      current$testB.reader1$tpf)
+# lines(current$testB.reader2$fpf,
+#      current$testB.reader2$tpf)
+# lines(current$testB.reader3$fpf,
+#       current$testB.reader3$tpf)
+# lines(current$testB.reader4$fpf,
+#       current$testB.reader4$tpf)
